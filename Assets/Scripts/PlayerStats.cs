ff20fbdcb23cba55ad0aa3930ab995b5f9531272
junyium1 <elkihal.aziz@icloud.com>
2026-04-09 19:48:05 +0200

    ajout main menu, player stats scripts et logique de damage, vie, et game over

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,63 @@
+using Oculus.VoiceSDK.UX;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStats : MonoBehaviour
+{
+    [Header("Health")]
+    public int maxLives = 2;
+    public int currentLives;
+    public GameObject[] hearts;
+
+    [Header("Endurance")]
+    public float maxStamina = 100f;
+    public float currentStamina;
+    public float staminaRegenRate = 15f;
+    public Slider staminaBar;
+
+    private void Start()
+    {
+        currentLives = maxLives;
+        currentStamina = maxStamina;
+        UpdateHeartsUI();
+    }
+
+    private void Update()
+    {
+        if (currentStamina < maxStamina)
+        {
+            currentStamina += staminaRegenRate * Time.deltaTime;
+            if (currentStamina > maxStamina) currentStamina = maxStamina;
+            if (staminaBar != null) staminaBar.value = currentStamina / maxStamina;
+        }
+    }
+
+    public void TakeDamage()
+    {
+        currentLives--;
+        UpdateHeartsUI();
+        if (currentLives <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void UpdateHeartsUI()
+    {
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < currentLives)
+            {
+                hearts[i].SetActive(true);
+            }
+        }
+    }
+
+    private void Die()
+    {
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.EndRound("kek ur bad");
+        }
+    }
+}
